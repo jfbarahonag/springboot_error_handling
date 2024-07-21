e540eaa48bd35870deec6c48de255b2e5159573d
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-21 09:45:36 -0500

    first commit

---
+++ src/main/java/com/jfbarahonag/springboot/error/springboot_error_handling/SpringbootErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.jfbarahonag.springboot.error.springboot_error_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootErrorHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootErrorHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfbarahonag/springboot/error/springboot_error_handling/SpringbootErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfbarahonag.springboot.error.springboot_error_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
