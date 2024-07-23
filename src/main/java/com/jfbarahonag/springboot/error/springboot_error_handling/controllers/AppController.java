f9c4cf70f3e4dfeb2523c359c4d80a57dba0a15b
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-22 22:43:46 -0500

    changing the way to get if the user exists without calling the custom exceptions handler

--- src/main/java/com/jfbarahonag/springboot/error/springboot_error_handling/controllers/AppController.java
+++ src/main/java/com/jfbarahonag/springboot/error/springboot_error_handling/controllers/AppController.java
@@ -1,11 +1,13 @@
 package com.jfbarahonag.springboot.error.springboot_error_handling.controllers;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jfbarahonag.springboot.error.springboot_error_handling.exceptions.UserNotFoundException;
 import com.jfbarahonag.springboot.error.springboot_error_handling.models.domain.User;
 import com.jfbarahonag.springboot.error.springboot_error_handling.services.UserService;
 
@@ -28,9 +30,14 @@ public class AppController {
   }
 
   @GetMapping("users/{id}")
-  public User getUserById(@PathVariable Long id) {
-    User user = userService.findById(id)
-        .orElseThrow(() -> new UserNotFoundException("User ".concat(String.valueOf(id).concat(" not found"))));
-    return user;
+  public ResponseEntity<?> getUserById(@PathVariable Long id) {
+    
+    Optional<User> optional = userService.findById(id);
+
+    if (optional.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok().body(optional.orElseThrow());
   }
 }
